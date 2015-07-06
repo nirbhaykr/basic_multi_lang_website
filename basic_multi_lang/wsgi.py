a91ecf5cfae4165ddfb1744f97c5e92b7b1e6887
nirbhaykr <kumar.nirbhay101@gmail.com>
2015-07-06 13:44:03 +0530

    Removed Virtual env activation

--- basic_multi_lang/wsgi.py
+++ basic_multi_lang/wsgi.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-activate_this = "/home/phaniv/nirbhay/virtual_evn/tes/bin/activate_this.py"
-execfile(activate_this, dict(__file__=activate_this))
+#activate_this = "/home/phaniv/nirbhay/virtual_evn/tes/bin/activate_this.py"
+#execfile(activate_this, dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
-activate_this = "/home/phaniv/nirbhay/virtual_evn/tes/bin/activate_this.py"
-execfile(activate_this, dict(__file__=activate_this))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basic_multi_lang.settings")
 
 application = get_wsgi_application()
