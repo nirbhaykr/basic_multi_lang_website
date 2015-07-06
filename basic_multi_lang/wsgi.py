f6eea090756fed229fac2d0d15534207fa6b1c9a
Vineet <vineetdubey@letechnosoft.net>
2015-07-06 13:38:57 +0530

    first commit

---
+++ basic_multi_lang/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for basic_multi_lang project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+activate_this = "/home/phaniv/nirbhay/virtual_evn/tes/bin/activate_this.py"
+execfile(activate_this, dict(__file__=activate_this))
+
+from django.core.wsgi import get_wsgi_application
+activate_this = "/home/phaniv/nirbhay/virtual_evn/tes/bin/activate_this.py"
+execfile(activate_this, dict(__file__=activate_this))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basic_multi_lang.settings")
+
+application = get_wsgi_application()
